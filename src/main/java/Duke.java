6d6c69428fb1b59e810d2e42bfe04ca861251ab3
lpeize <e0261957@u.nus.edu>
2019-08-25 21:47:31 +0800

    Level-1,oopVersion

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,35 @@
 import java.util.Scanner;
 
+class Line {
+    String CurrLine;
+    void Enter() {
+        Scanner in = new Scanner(System.in);
+        CurrLine = in.nextLine();
+    }
+    boolean Judge() {
+        if (CurrLine.equals("bye")) return true;
+        else return false;
+    }
+    void Echo() {
+        System.out.println("\t" + CurrLine);
+    }
+    void Goodbye() {
+        System.out.println("\tBye. Hope to see you again soon!");
+    }
+}
+
 public class Duke {
     public static void main(String args[]) {
         System.out.println("\tHello! I'm Duke\n\tWhat can I do for you?");
-        Scanner in = new Scanner(System.in);
-        String temp = null;
-        temp = in.nextLine();
-        while(!temp.equals("bye")) {
-            System.out.println("\t" + temp);
-            temp = in.nextLine();
+        Line OneLine = new Line();
+        //enter
+        OneLine.Enter();
+        //test this line
+        while (OneLine.Judge() == false) {
+            OneLine.Echo();
+            OneLine.Enter();
         }
-        System.out.println("\tBye. Hope to see you again soon!");
+        //goodbye
+        OneLine.Goodbye();
     }
 }
