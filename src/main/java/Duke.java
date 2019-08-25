5f51dd95180b727ebb8d5e7424fafd6f8794092f
lpeize <e0261957@u.nus.edu>
2019-08-25 22:32:01 +0800

    Level-2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,34 @@
 import java.util.Scanner;
 
-class Line {
+class Lines {
     private String currLine;
+    private String[] theList = new String[100];
+    private int num = 0;
+    void Numincrease() {
+        num++;
+    }
+    void Add() {
+        theList[num] = currLine;
+        Numincrease();
+    }
     void Enter() {
         Scanner in = new Scanner(System.in);
         currLine = in.nextLine();
+        if (!currLine.equals("list")) Add();
     }
-    boolean Judge() {
+    void Showlist() {
+        for (int i = 0; i < num; ++i) {
+            System.out.println("\t" + (i+1) + ". " + theList[i]);
+        }
+    }
+    boolean process() {
         if (currLine.equals("bye")) return true;
-        else return false;
+        else if (currLine.equals("list")) Showlist();
+        else Echo();
+        return false;
     }
     void Echo() {
-        System.out.println("\t" + currLine);
+        System.out.println("\tadded: " + currLine);
     }
     void Goodbye() {
         System.out.println("\tBye. Hope to see you again soon!");
@@ -21,12 +38,11 @@ class Line {
 public class Duke {
     public static void main(String args[]) {
         System.out.println("\tHello! I'm Duke\n\tWhat can I do for you?");
-        Line oneLine = new Line();
+        Lines oneLine = new Lines();
         //enter
         oneLine.Enter();
         //test this line
-        while (oneLine.Judge() == false) {
-            oneLine.Echo();
+        while (!oneLine.process()) {
             oneLine.Enter();
         }
         //goodbye
