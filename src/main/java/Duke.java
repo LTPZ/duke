d99e5f7ec862bf545467bbbfceccc9173dca170d
lpeize <e0261957@u.nus.edu>
2019-08-25 21:07:08 +0800

    level-1,second_version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,10 +5,11 @@ public class Duke {
         System.out.println("\tHello! I'm Duke\n\tWhat can I do for you?");
         Scanner in = new Scanner(System.in);
         String temp = null;
-        do {
-            temp = in.nextLine();
+        temp = in.nextLine();
+        while(!temp.equals("bye")) {
             System.out.println("\t" + temp);
-        } while(!temp.equals("bye"));
+            temp = in.nextLine();
+        }
         System.out.println("\tBye. Hope to see you again soon!");
     }
 }
