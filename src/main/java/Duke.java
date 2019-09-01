08a84204ec41161a7ec708a80b8431283e47f1e1
lpeize <e0261957@u.nus.edu>
2019-09-01 18:16:20 +0800

    Level-9. Find

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,29 +11,17 @@ public class Duke {
         + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("\tHello! I'm Duke\n\tWhat can I do for you?");
-<<<<<<< HEAD
         //load data from the file
         TaskList taskList = new TaskList(0);
-=======
-        //load data
-        TaskList tasklist = new TaskList(0);
->>>>>>> branch-Level-9
         File list = new File("D:/NUS/IDEs/Du/List.txt");
         BufferedReader reader = new BufferedReader(new FileReader(list));
         String oldData = null;
         while((oldData = reader.readLine()) != null) {
             //process old data
-<<<<<<< HEAD
             taskList.oldDataProcess(oldData);
         }
         reader.close();
         //process the new command line
         taskList.scanProcess();
-=======
-            tasklist.oldDataProcess(oldData);
-        }
-        reader.close();
-        tasklist.scantoProcess();
->>>>>>> branch-Level-9
     }
 }
