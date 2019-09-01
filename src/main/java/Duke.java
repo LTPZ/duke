113decd6232cfe3bf1f67648b659315d5ebc3faa
lpeize <e0261957@u.nus.edu>
2019-09-01 18:12:20 +0800

    Level-9. Find

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,9 @@
 import java.io.*;
 import java.text.ParseException;
-import java.util.*;
 
 public class Duke {
     public static void main(String args[]) throws IOException, ParseException {
+        //say hello
         String logo = " ____        _        \n"
         + "|  _ \\ _   _| | _____ \n"
         + "| | | | | | | |/ / _ \\\n"
@@ -11,16 +11,29 @@ public class Duke {
         + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("\tHello! I'm Duke\n\tWhat can I do for you?");
+<<<<<<< HEAD
+        //load data from the file
+        TaskList taskList = new TaskList(0);
+=======
         //load data
         TaskList tasklist = new TaskList(0);
+>>>>>>> branch-Level-9
         File list = new File("D:/NUS/IDEs/Du/List.txt");
         BufferedReader reader = new BufferedReader(new FileReader(list));
         String oldData = null;
         while((oldData = reader.readLine()) != null) {
             //process old data
+<<<<<<< HEAD
+            taskList.oldDataProcess(oldData);
+        }
+        reader.close();
+        //process the new command line
+        taskList.scanProcess();
+=======
             tasklist.oldDataProcess(oldData);
         }
         reader.close();
         tasklist.scantoProcess();
+>>>>>>> branch-Level-9
     }
 }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,18 +1,18 @@
-import jdk.jfr.Event;
-
 import java.io.*;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+
 import java.util.*;
 
 public class TaskList {
     protected String currCommand;
-    protected List<Tasks> list;
+    protected List<Task> list;
     protected int size;
 
     public TaskList(int size) {
         this.size = size;
-        this.list = new ArrayList<Tasks>();
+        this.list = new ArrayList<>();
     }
 
     public void oldDataProcess(String s) throws ParseException {
@@ -27,7 +27,7 @@ public class TaskList {
                 type = "deadline";
                 break;
             default:
-                type = "events";
+                type = "event";
         }
         boolean isDone;
         char d = s.charAt(4);
@@ -35,170 +35,169 @@ public class TaskList {
         //kill the |
         String content = s.substring(s.indexOf(" | ") + 3);
         content = content.substring(s.indexOf(" |") + 3);
-        String time = null;
+        String time;
         Date date = null;
         if (content.contains(" | ")) {
             //process as event or deadline
-            time = content.substring(content.indexOf(" | ")+3);
-            content = content.substring(0,content.indexOf(" | "));
-            date = new SimpleDateFormat("dd/mm/yyyy").parse(time);
+            time = content.substring(content.indexOf(" | ") + 3);
+            content = content.substring(0, content.indexOf(" | "));
+            date = new SimpleDateFormat("dd/MM/yyyy").parse(time);
         }
-        if (type == "todo") list.add(new Tasks(content, isDone, type));
+        if (type == "todo") list.add(new Task(content, isDone, type));
         else if (type == "deadline") list.add(new Deadline(content, isDone, type, date));
-        else list.add(new Events(content, isDone, type, date));
+        else list.add(new Even(content, isDone, type, date));
         size++;
     }
 
-    public void scantoProcess() {
+    public void scanProcess() {
         Scanner in = new Scanner(System.in);
         currCommand = in.nextLine();
         try {
             if (check().equals("bye")) {
                 //exit
-                goodbye();
+                sayGoodbye();
             } else if (check().equals("done")) {
                 //done the task in the list
                 doneTask();
-                scantoProcess();
+                scanProcess();
             } else if (check().equals("list")) {
                 //list the task in the list
                 listTask();
-                scantoProcess();
+                scanProcess();
             } else if (check().equals("todo")) {
                 //add
-                size = addToDo();
-                scantoProcess();
+                addToDo();
+                scanProcess();
             } else if (check().equals("deadline")) {
                 //add
-                size = addDDL();
-                scantoProcess();
+                addDeadline();
+                scanProcess();
             } else if (check().equals("event")){
                 //event
-                size = addEvent();
-                scantoProcess();
+                addEvent();
+                scanProcess();
             } else {
                 //find
                 findKey();
-                scantoProcess();
+                scanProcess();
             }
         } catch (EntryException e) {
             System.out.println("\t☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-            scantoProcess();
+            scanProcess();
         }
     }
 
-    private String check() throws EntryException{
+    private String check() throws EntryException {
         StringTokenizer tok = new StringTokenizer(currCommand);
         String command1 = tok.nextToken();
-        if (!command1.equals("bye") && !command1.equals("done") && !command1.equals("list")
-                && !command1.equals("todo") && !command1.equals("deadline") && !command1.equals("event")
-                && !command1.equals("find"))
+        if (!command1.equals("bye") && !command1.equals("done") &&
+                !command1.equals("list") && !command1.equals("todo") && !command1.equals("deadline")
+                && !command1.equals("event") && !command1.equals("find"))
             throw new EntryException();
         return command1;
     }
 
-    private void checkLackInfo() throws EntryException {
-        StringTokenizer tok = new StringTokenizer(currCommand);
-        String tmp = tok.nextToken();
-        if (!tok.hasMoreElements()) {
-            throw new EntryException();
-        }
-    }
-
-    private void checkDeadlineTime() throws TimeException {
-        if (currCommand.indexOf(" /by") == -1) throw new TimeException();
-    }
-
-    private void checkEventTime() throws TimeException {
-        if (currCommand.indexOf(" /at") == -1) throw new TimeException();
+    private void sayGoodbye() {
+        System.out.println("\tBye. Hope to see you again soon!");
     }
 
     private void doneTask() {
         StringTokenizer tok = new StringTokenizer(currCommand);
-        String command1 = tok.nextToken();
+        String temp = tok.nextToken();
         try {
+            //check if more information is needed
             checkLackInfo();
-            String numinString = tok.nextToken();
-            int num = Integer.parseInt(numinString);
-            int i;
-            for (i = 0; i < num - 1; i++) ;
-            list.get(i).done();
+            //search for the done number
+            String numText = tok.nextToken();
+            int num = Integer.parseInt(numText) - 1;
+            //done the one in list
+            list.get(num).setDone();
             System.out.println("\tNice! I've marked this task as done:");
-            if (list.get(i).getType().equals("todo")) System.out.println("\t[T]" + list.get(i).toString());
-            else System.out.println("\t" + list.get(i).toString());
-            try {
-                File currList = new File("D:/NUS/IDEs/Du/List.txt");
-                //update the done information by reading and writing to a temp file
-                BufferedReader reader = new BufferedReader(new FileReader(currList));
-                File data = new File("D:/NUS/IDEs/Du/List2.txt");
-                FileWriter newData = new FileWriter(data);
-                PrintWriter pw = new PrintWriter(newData);
-                String oldData = null;
-                for (int j = 0; j < i; j++) {
-                    oldData = reader.readLine();
-                    pw.println(oldData);
-                }
-                oldData = reader.readLine();
-                pw.println(oldData.substring(0,4) + "1" + oldData.substring(5));
-                while ((oldData = reader.readLine()) != null) {
-                    pw.println(oldData);
-                }
-                reader.close();
-                pw.flush();
-                pw.close();
-                //print back
-                currList = new File("D:/NUS/IDEs/Du/List2.txt");
-                reader = new BufferedReader(new FileReader(currList));
-                data = new File("D:/NUS/IDEs/Du/List.txt");
-                newData = new FileWriter(data);
-                pw = new PrintWriter(newData);
-                oldData = null;
-                while ((oldData = reader.readLine()) != null) {
-                    //copy everything back
-                    pw.println(oldData);
-                }
-                reader.close();
-                pw.flush();
-                pw.close();
-            } catch (Exception e) {
-                System.out.println(e);
+            if (list.get(num).getType().equals("todo")) {
+                //it is a common type
+                System.out.println("\t[T]" + list.get(num).toString());
+            } else {
+                //deadline or event type
+                System.out.println("\t" + list.get(num).toString());
             }
+            //print the change in the file
+            changeFile(num, "done");
         } catch (EntryException e) {
-            System.out.println("\t☹ OOPS!!! I cannot done an empty task");
+            System.out.println("\t☹ OOPS!!! I cannot delete an empty task, specific number needed");
         }
     }
 
     private void listTask() {
         System.out.println("\tHere are the tasks in your list:");
         for (int i = 0; i < size; i++) {
-            if (list.get(i).getType().equals("todo")) System.out.println("\t" + (i + 1) + ".[T]" + list.get(i).toString());
+            if (list.get(i).getType().equals("todo"))
+                System.out.println("\t" + (i + 1) + ".[T]" + list.get(i).toString());
             else System.out.println("\t" + (i + 1) + "." + list.get(i).toString());
         }
     }
-    private void todofileWrite(Tasks ta) throws IOException {
-        File data = new File("D:/NUS/IDEs/Du/List.txt");
-        FileWriter newData = new FileWriter(data, true);
-        PrintWriter pw = new PrintWriter(newData);
-        pw.print("T | " + (ta.isDone()? 1:0) + " |" + ta.getDescription() + "\r\n");
-        pw.flush();
-        pw.close();
+
+    private void addToDo() {
+        try {
+            checkLackInfo();
+            String content = currCommand.substring(currCommand.indexOf(" ") + 1);
+            Task currTask = new Task(content, "todo");
+            list.add(currTask);
+            System.out.println("\tGot it. I've added this task:");
+            System.out.println("\t " + list.get(size).toString());
+            System.out.println("\tNow you have " + (size + 1) + " tasks in the list.");
+            addFile(currTask, "todo");
+            size++;
+        } catch (EntryException e) {
+            System.out.println("\t☹ OOPS!!! The description of a todo cannot be empty.");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 
-    private void deadlinefileWrite(Deadline dd) throws IOException {
-        File data = new File("D:/NUS/IDEs/Du/List.txt");
-        FileWriter newData = new FileWriter(data, true);
-        PrintWriter pw = new PrintWriter(newData);
-        pw.print("D | " + (dd.isDone()? 1:0) + " |" + dd.getDescription() + " | " + dd.getTime() + "\r\n");
-        pw.flush();
-        pw.close();
+    private void addDeadline() {
+        try {
+            checkLackInfo();
+            checkDeadlineTime();
+            String content = currCommand.substring(currCommand.indexOf(" ") + 1, currCommand.indexOf(" /by"));
+            String time = currCommand.substring(currCommand.indexOf(" /by") + 5);
+            Date by = new SimpleDateFormat("dd/MM/yyyy").parse(time);
+            Deadline deadline = new Deadline(content, false, "deadline", by, time);
+            list.add(deadline);
+            System.out.println("\tGot it. I've added this task:");
+            System.out.println("\t " + list.get(size).toString());
+            System.out.println("\tNow you have " + (size + 1) + " tasks in the list.");
+            addFile(deadline,"deadline");
+            size++;
+        } catch (EntryException e) {
+            System.out.println("\t☹ OOPS!!! The description of a deadline cannot be empty.");
+        } catch (TimeException e) {
+            System.out.println("\t☹ OOPS!!! The time of a deadline cannot be empty.");
+        } catch (Exception e) {
+            System.out.println("\t☹ OOPS!!! The time of a deadline should be in \"dd/mm/yyyy form\".");
+        }
     }
-    private void eventfileWrite(Events ev) throws IOException {
-        File data = new File("D:/NUS/IDEs/Du/List.txt");
-        FileWriter newData = new FileWriter(data, true);
-        PrintWriter pw = new PrintWriter(newData);
-        pw.print("E | " + (ev.isDone()? 1:0) + " |" + ev.getDescription() + " | " + ev.getTime()+ "\r\n");
-        pw.flush();
-        pw.close();
+
+    private void addEvent() {
+        try {
+            checkLackInfo();
+            checkEventTime();
+            String content = currCommand.substring(currCommand.indexOf(" ") + 1, currCommand.indexOf(" /at"));
+            String time = currCommand.substring(currCommand.indexOf(" /at") + 5);
+            Date at = new SimpleDateFormat("dd/MM/yyyy").parse(time);
+            Even event = new Even(content, false, "event", at, time);
+            list.add(event);
+            System.out.println("\tGot it. I've added this task:");
+            System.out.println("\t " + list.get(size).toString());
+            System.out.println("\tNow you have " + (size + 1) + " tasks in the list.");
+            addFile(event,"Event");
+            size++;
+        } catch (EntryException e) {
+            System.out.println("\t☹ OOPS!!! The description of an event cannot be empty.");
+        } catch (TimeException e) {
+            System.out.println("\t☹ OOPS!!! The time of an event cannot be empty.");
+        } catch (Exception e) {
+            System.out.println("\t☹ OOPS!!! The time of a event should be in \"dd/mm/yyyy form\".");
+        }
     }
 
     private void findKey() {
@@ -225,75 +224,97 @@ public class TaskList {
         }
     }
 
-    private  int addToDo() {
+    private void addFile(Task currTask, String Type) throws IOException {
+        File data = new File("D:/NUS/IDEs/Du/List.txt");
+        FileWriter newData = new FileWriter(data, true);
+        PrintWriter pw = new PrintWriter(newData);
+        if (Type.equals("todo")) {
+            pw.print("T | " + (currTask.isDone() ? 1 : 0) + " |" + currTask.getDescription() + "\r\n");
+        } else if (Type.equals("deadline")) {
+            Deadline deadline = (Deadline) currTask;
+            pw.print("D | " + (deadline.isDone() ? 1 : 0) + " |" + deadline.getDescription() + " | " + deadline.getTime() + "\r\n");
+        } else {
+            //event
+            Even event = (Even) currTask;
+            pw.print("E | " + (event.isDone() ? 1 : 0) + " |" + event.getDescription() + " | " + event.getTime() + "\r\n");
+        }
+        pw.flush();
+        pw.close();
+    }
+
+    private void changeFile(int changedIndex, String Type) {
+        //this is used to delete or done tasks and print them in the file
         try {
-            checkLackInfo();
-            String content = currCommand.substring(currCommand.indexOf(" "));
-            list.add(new Tasks(content, "todo"));
-            System.out.println("\tGot it. I've added this task:");
-            System.out.println("\t " + list.get(size).toString());
-            System.out.println("\tNow you have " + (size + 1) + " tasks in the list.");
-            todofileWrite(new Tasks(content, "todo"));
-            size++;
-        } catch (EntryException e) {
-            System.out.println("\t☹ OOPS!!! The description of a todo cannot be empty.");
-        } finally {
-            return size;
+            File currList = new File("D:/NUS/IDEs/Du/List.txt");
+            //copy list 1 to temp list 2 and make the changes
+            BufferedReader reader = new BufferedReader(new FileReader(currList));
+            File data = new File("D:/NUS/IDEs/Du/List2.txt");
+            FileWriter newData = new FileWriter(data);
+            PrintWriter pw = new PrintWriter(newData);
+            String oldData;
+            for (int j = 0; j < changedIndex; j++) {
+                oldData = reader.readLine();
+                pw.println(oldData);
+            }
+            oldData = reader.readLine();
+            //make the change
+            if (Type == "done") {
+                //change the done part to 1
+                pw.println(oldData.substring(0, 4) + "1" + oldData.substring(5));
+            } else if (Type == "delete") {
+                //do not print this line so this line will appear "deleted" in the list
+            }
+            //continue copy the remaining items
+            while ((oldData = reader.readLine()) != null) {
+                pw.println(oldData);
+            }
+            reader.close();
+            pw.flush();
+            pw.close();
+            //copy list 2 contents to list 1
+            currList = new File("D:/NUS/IDEs/Du/List2.txt");
+            reader = new BufferedReader(new FileReader(currList));
+            data = new File("D:/NUS/IDEs/Du/List.txt");
+            newData = new FileWriter(data);
+            pw = new PrintWriter(newData);
+            oldData = null;
+            while ((oldData = reader.readLine()) != null) {
+                //copy everything back
+                pw.println(oldData);
+            }
+            reader.close();
+            pw.flush();
+            pw.close();
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
 
-    private  int addDDL() {
-        try {
-            checkLackInfo();
-            checkDeadlineTime();
-            String content = currCommand.substring(currCommand.indexOf(" "), currCommand.indexOf(" /by"));
-            String time = currCommand.substring(currCommand.indexOf(" /by") + 5);
-            Date by = new SimpleDateFormat("dd/mm/yyyy").parse(time);
-            list.add(new Deadline(content, false, "deadline", by, time));
-            System.out.println("\tGot it. I've added this task:");
-            System.out.println("\t " + list.get(size).toString());
-            System.out.println("\tNow you have " + (size + 1) + " tasks in the list.");
-            deadlinefileWrite(new Deadline(content,false, "deadline", by, time));
-            size++;
-        } catch (EntryException e) {
-            System.out.println("\t☹ OOPS!!! The description of a deadline cannot be empty.");
-        } catch (TimeException e) {
-            System.out.println("\t☹ OOPS!!! The time of a deadline cannot be empty.");
-        } finally {
-            return size;
-        }
-    }
-    private  int addEvent() {
-        try {
-            checkLackInfo();
-            checkEventTime();
-            String content = currCommand.substring(currCommand.indexOf(" "), currCommand.indexOf(" /at"));
-            String time = currCommand.substring(currCommand.indexOf(" /by") + 5);
-            Date at = new SimpleDateFormat("dd/mm/yyyy").parse(time);
-            list.add(new Events(content, false, "events", at, time));
-            System.out.println("\tGot it. I've added this task:");
-            System.out.println("\t " + list.get(size).toString());
-            System.out.println("\tNow you have " + (size + 1) + " tasks in the list.");
-            eventfileWrite(new Events(content, false, "events", at, time));
-            size++;
-        } catch (EntryException e) {
-            System.out.println("\t☹ OOPS!!! The description of an event cannot be empty.");
-        } catch (TimeException e) {
-            System.out.println("\t☹ OOPS!!! The time of an event cannot be empty.");
-        } finally {
-            return size;
+    private void checkLackInfo() throws EntryException {
+        StringTokenizer tok = new StringTokenizer(currCommand);
+        String tmp = tok.nextToken();
+        if (!tok.hasMoreElements()) {
+            throw new EntryException();
         }
     }
 
-    private void goodbye() {
-        System.out.println("\tBye. Hope to see you again soon!");
+    private void checkDeadlineTime() throws Exception {
+        //check if there is no time
+        if (currCommand.indexOf(" /by") == -1) {
+            throw new TimeException();
+        }
     }
-}
 
-class EntryException extends Exception {
+    private void checkEventTime() throws Exception {
+        //check if there is no time
+        if (currCommand.indexOf(" /at") == -1) {
+            throw new TimeException();
+        }
+    }
 
-}
+    class TimeException extends Exception {
+    }
 
-class TimeException extends Exception {
-
+    class EntryException extends Exception {
+    }
 }
